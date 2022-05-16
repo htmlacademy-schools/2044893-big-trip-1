0d17be96c6b0d7e12dff6c40c8591f04c6ecd868
Maikenov <pepsisay2015@gmail.com>
2022-05-16 15:31:10 +0500

    commit2

--- src/view/site-add-first-point-view.js
+++ src/view/site-add-first-point-view.js
@@ -1,27 +1,22 @@
 import { createElement } from '../render.js';
 
 const createFirstPointTemplate = () => {
-    `<p class="trip-events__msg">
-    Click New Event to create your first point
-    </p>`
-}
-
+  `<p class="trip-events__msg">
+  Click New Event to create your first point
+  </p>`
+};
 export default class iteAddFirstPoint {
   #element = null;
-  
   get element() {
     if (!this.#element) {
       this.#element = createElement(this.template);
-      }
-  
-      return this.#element;
-    }
-  
-    get template() {
-      return createFirstPointTemplate();
-    }
-  
-    removeElement() {
-      this.#element = null;
-    }
+    }  
+    return this.#element;
+  }  
+  get template() {
+    return createFirstPointTemplate();
+  }
+  removeElement() {
+    this.#element = null;
+  }
 }
